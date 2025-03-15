53c6e995cdbb1dfce4b8ffffd1f7d6d18468c4d7
surajjha-37 <diveshchoudhary497@gmail.com>
2025-03-15 10:17:53 +0530

    edit html file

--- javascript/cal/script.js
+++ javascript/cal/script.js
@@ -12,7 +12,10 @@ function add() {
   console.log(typeof in1value);
   let in2value = Number(in2.value);
   let sum = in1value + in2value;
-  output.innerHTML = sum;
+  output.innerHTML = "The sum of two no. is   =   " + sum;
+  output.classList.add("css");
+  outputdiv.classList.add("cssdiv");
+  confetti();
 }
 btn1.addEventListener("click", add);
 
@@ -21,7 +24,10 @@ function sub() {
   console.log(typeof in1value);
   let in2value = Number(in2.value);
   let sub = in1value - in2value;
-  output.innerHTML = sub;
+  output.innerHTML = "The difference of two no. is   =   " + sub;
+  output.classList.add("css");
+  outputdiv.classList.add("cssdiv");
+  confetti();
 }
 btn2.addEventListener("click", sub);
 
@@ -30,7 +36,10 @@ function mul() {
   console.log(typeof in1value);
   let in2value = Number(in2.value);
   let mul = in1value * in2value;
-  output.innerHTML = mul;
+  output.innerHTML = "The multiple of two no. is   =   " + mul;
+  output.classList.add("css");
+  outputdiv.classList.add("cssdiv");
+  confetti();
 }
 btn3.addEventListener("click", mul);
 
@@ -39,6 +48,33 @@ function div() {
   console.log(typeof in1value);
   let in2value = Number(in2.value);
   let div = in1value / in2value;
-  output.innerHTML = div;
+  output.innerHTML = "The divide of two no. is   =   " + div;
+  output.classList.add("css");
+  outputdiv.classList.add("cssdiv");
+  confetti();
 }
 btn4.addEventListener("click", div);
+
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let outputdiv2 = document.querySelector(".outputdiv2");
+let output2 = document.querySelector(".output2");
+let in3 = document.querySelector("#in-3");
+
+function convert() {
+  let in3value = in3.value;
+  output2.innerHTML = in3value * 100 + "   cm";
+  output2.classList.add("css");
+  outputdiv2.classList.add("cssdiv");
+  confetti();
+}
+btn5.addEventListener("click", convert);
+
+function convert2() {
+  let in3value = in3.value;
+  output2.innerHTML = in3value / 100 + "  m";
+  output2.classList.add("css");
+  outputdiv2.classList.add("cssdiv");
+  confetti();
+}
+btn6.addEventListener("click", convert2);
