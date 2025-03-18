f8d982e4b98a2673b904c7d742a748005300bc46
surajjha-37 <diveshchoudhary497@gmail.com>
2025-03-18 20:50:34 +0530

    add file

---
+++ javascript/random/script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector(".btn");
+
+function change() {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+}
+btn.addEventListener("click", change);
