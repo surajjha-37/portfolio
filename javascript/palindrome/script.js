1c29faf479431bc80318ee21d5eaa793b1243909
surajjha-37 <diveshchoudhary497@gmail.com>
2025-03-22 20:54:56 +0530

    edit html css and js file

---
+++ javascript/palindrome/script.js
@@ -0,0 +1,16 @@
+let input = document.querySelector(".in-1");
+let btn = document.querySelector(".btn");
+let output = document.querySelector(".output");
+
+function palin() {
+  let invalue = input.value;
+  let lower = invalue.toLowerCase();
+  let reverse = lower.split("").reverse().join("");
+  output.classList.add("css");
+  if (invalue === reverse) {
+    output.innerHTML = "Yes, Your Name is Palindrome";
+  } else {
+    output.innerHTML = "No, Your Name is not Palindrome";
+  }
+}
+btn.addEventListener("click", palin);
