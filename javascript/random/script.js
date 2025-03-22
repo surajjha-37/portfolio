5ec364106883d02fc73611cbc4d9eb3e8b9a1c8a
surajjha-37 <diveshchoudhary497@gmail.com>
2025-03-22 19:26:34 +0530

    edit js file

--- javascript/random/script.js
+++ javascript/random/script.js
@@ -1,7 +1,8 @@
-// let btn = document.querySelector(".btn");
+let btn = document.querySelector(".btn");
 
-// function change() {
-//   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-//   document.body.style.backgroundColor = randomColor;
-// }
-// btn.addEventListener("click", change);
+function change() {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+}
+
+btn.addEventListener("click", change);
